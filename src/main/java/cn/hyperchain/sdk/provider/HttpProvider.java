b7db9d47d34e7484309d56a6b6639a523b966d2a
Qi He <heqi@hyperchain.cn>
2019-03-14 15:10:42 +0800

    fix provider

--- src/main/java/cn/hyperchain/sdk/provider/HttpProvider.java
+++ src/main/java/cn/hyperchain/sdk/provider/HttpProvider.java
@@ -11,6 +11,7 @@ import java.util.Map;
  * @date: 2019-03-12
  */
 
+// 每一种协议一个接口，每种协议可以有多个具体实现方式
 public interface HttpProvider {
     String post(String body, Map<String, String> headers) throws RequestException;
 
--- src/main/java/cn/hyperchain/sdk/provider/Provider.java
+++ src/main/java/cn/hyperchain/sdk/provider/Provider.java
@@ -1,50 +1,29 @@
 package cn.hyperchain.sdk.provider;
 
+import cn.hyperchain.sdk.request.Request;
+import cn.hyperchain.sdk.response.Response;
+
 import java.io.IOException;
 import java.util.concurrent.Future;
 
+
+// 由于ProviderManager的需要，此Provider可能无法抽象支持多种连接方式，暂时保留，每一种协议一个接口，每种协议可以有多个实现方式
 public interface Provider {
-//    <T extends Response> T send(
-//            Request request, Class<T> responseType) throws IOException;
-//
-//    /**
-//     * Performs an asynchronous JSON-RPC request.
-//     *
-//     * @param request      request to perform
-//     * @param responseType class of a data item returned by the request
-//     * @param <T>          type of a data item returned by the request
-//     * @return CompletableFuture that will be completed when a result is returned or if a
-//     * request has failed
-//     */
-//    Future<T> sendAsync(Request request, Class<T> responseType);
-//
-//    /**
-//     * Subscribe to a stream of notifications. A stream of notifications is opened by
-//     * by performing a specified JSON-RPC request and is closed by calling
-//     * the unsubscribe method. Different WebSocket implementations use different pair of
-//     * subscribe/unsubscribe methods.
-//     *
-//     * <p>This method creates an Flowable that can be used to subscribe to new notifications.
-//     * When a client unsubscribes from this Flowable the service unsubscribes from
-//     * the underlying stream of events.
-//     *
-//     * @param request           JSON-RPC request that will be send to subscribe to a stream of
-//     *                          events
-//     * @param unsubscribeMethod method that will be called to unsubscribe from a
-//     *                          stream of notifications
-//     * @param responseType      class of incoming events objects in a stream
-//     * @param <T>               type of incoming event objects
-//     * @return a {@link Flowable} instance that emits incoming events
-//     */
+    <T extends Response> T send(Request request, Class<T> responseType) throws IOException;
+
+
+    <T extends Response> Future<T> sendAsync(Request request, Class<T> responseType);
+
+
 //    <T extends Notification<?>> Flowable<T> subscribe(
 //            Request request,
 //            String unsubscribeMethod,
 //            Class<T> responseType);
-//
-//    /**
-//     * Closes resources used by the service.
-//     *
-//     * @throws IOException thrown if a service failed to close all resources
-//     */
-//    void close() throws IOException;
+
+    /**
+     * Closes resources used by the service.
+     *
+     * @throws IOException thrown if a service failed to close all resources
+     */
+    void close() throws IOException;
 }
