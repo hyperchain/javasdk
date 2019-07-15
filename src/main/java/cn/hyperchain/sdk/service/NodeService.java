eb62dd891d17c1f2c081a6b3ded8492619b61782
dongjianhui <dongjianhui@hyperchain.cn>
2019-07-15 14:47:25 +0800

    feat(node): add node service

--- src/main/java/cn/hyperchain/sdk/service/NodeService.java
+++ src/main/java/cn/hyperchain/sdk/service/NodeService.java
@@ -1,8 +1,34 @@
 package cn.hyperchain.sdk.service;
 
 import cn.hyperchain.sdk.request.Request;
+import cn.hyperchain.sdk.response.node.NodeHashResponse;
 import cn.hyperchain.sdk.response.node.NodeResponse;
+import cn.hyperchain.sdk.response.node.NodeStateResponse;
 
 public interface NodeService {
-    Request<NodeResponse> getNodes(int... ids);
+    Request<NodeResponse> getNodes(int... nodeIds);
+
+    /**
+     * get all nodes states.
+     *
+     * @param nodeIds specific ids
+     * @return {@link Request} of {@link NodeStateResponse}
+     */
+    Request<NodeStateResponse> getNodeStates(int... nodeIds);
+
+    /**
+     * get hash of the node that select in nodeIds randomly.
+     *
+     * @param nodeIds specific ids
+     * @return {@link Request} of {@link NodeHashResponse}
+     */
+    Request<NodeHashResponse> getNodeHash(int... nodeIds);
+
+    /**
+     * get hash of the node by nodeId.
+     *
+     * @param nodeId specific id
+     * @return {@link Request} of {@link NodeHashResponse}
+     */
+    Request<NodeHashResponse> getNodeHashByID(int nodeId);
 }
--- src/main/java/cn/hyperchain/sdk/service/impl/NodeServiceImpl.java
+++ src/main/java/cn/hyperchain/sdk/service/impl/NodeServiceImpl.java
@@ -3,7 +3,9 @@ package cn.hyperchain.sdk.service.impl;
 import cn.hyperchain.sdk.provider.ProviderManager;
 import cn.hyperchain.sdk.request.NodeRequest;
 import cn.hyperchain.sdk.request.Request;
+import cn.hyperchain.sdk.response.node.NodeHashResponse;
 import cn.hyperchain.sdk.response.node.NodeResponse;
+import cn.hyperchain.sdk.response.node.NodeStateResponse;
 import cn.hyperchain.sdk.service.NodeService;
 
 public class NodeServiceImpl implements NodeService {
@@ -15,8 +17,26 @@ public class NodeServiceImpl implements NodeService {
     }
 
     @Override
-    public Request<NodeResponse> getNodes(int... ids) {
-        NodeRequest nodeRequest = new NodeRequest(NODE_PREFIX + "getNodes", providerManager, NodeResponse.class, ids);
+    public Request<NodeResponse> getNodes(int... nodeIds) {
+        NodeRequest nodeRequest = new NodeRequest(NODE_PREFIX + "getNodes", providerManager, NodeResponse.class, nodeIds);
+        return nodeRequest;
+    }
+
+    @Override
+    public Request<NodeStateResponse> getNodeStates(int... nodeIds) {
+        NodeRequest nodeRequest = new NodeRequest(NODE_PREFIX + "getNodeStates", providerManager, NodeStateResponse.class, nodeIds);
+        return nodeRequest;
+    }
+
+    @Override
+    public Request<NodeHashResponse> getNodeHash(int... nodeIds) {
+        NodeRequest nodeRequest = new NodeRequest(NODE_PREFIX + "getNodeHash", providerManager, NodeHashResponse.class, nodeIds);
+        return nodeRequest;
+    }
+
+    @Override
+    public Request<NodeHashResponse> getNodeHashByID(int nodeId) {
+        NodeRequest nodeRequest = new NodeRequest(NODE_PREFIX + "getNodeHash", providerManager, NodeHashResponse.class, nodeId);
         return nodeRequest;
     }
 }
