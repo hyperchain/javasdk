1d8f0d99a098987863f9ab9aa853c1a444371719
Yeqi Tao <taoyeqi@hyperchain.cn>
2021-01-07 20:11:12 +0800

    style(*): format code

--- src/main/java/cn/hyperchain/sdk/common/utils/MethodNameUtil.java
+++ src/main/java/cn/hyperchain/sdk/common/utils/MethodNameUtil.java
@@ -4,12 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MethodNameUtil {
+
     /**
-     * get normalized method name,
+     * get normalized method name.
      * for example:
-     * if we have "testIntAndUint(int,uint,uint256,int[2])" for input,
-     * we will get "testIntAndUint(int256,uint256,uint256,int256[2])" for output
-     * @param methodName
+     * if we have "testIntAndUint(int,uint,uint256,int[2])" for input, we will get "testIntAndUint(int256,uint256,uint256,int256[2])" for output
+     * @param methodName method name
      * @return normalized methodName
      */
     public static String getNormalizedMethodName(String methodName) {
@@ -18,7 +18,7 @@ public class MethodNameUtil {
         Pattern p = Pattern.compile("int[^0-9]");
         Matcher m = p.matcher(methodName.substring(firstBracketIndex));
         StringBuffer sb = new StringBuffer();
-        while(m.find()) {
+        while (m.find()) {
             m.appendReplacement(sb, "int256" + m.group().substring(3));
         }
         m.appendTail(sb);
