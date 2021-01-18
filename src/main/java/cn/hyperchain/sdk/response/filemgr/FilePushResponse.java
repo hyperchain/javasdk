5e98d5e82f9990509b824d80604c0a2060c807fa
Peng Jin <jinpeng@hyperchain.cn>
2021-01-18 10:43:24 +0800

    fix #QAQC-132, fix filemgr bugs

--- src/main/java/cn/hyperchain/sdk/response/filemgr/FilePushResponse.java
+++ src/main/java/cn/hyperchain/sdk/response/filemgr/FilePushResponse.java
@@ -1,8 +1,12 @@
 package cn.hyperchain.sdk.response.filemgr;
 
 import cn.hyperchain.sdk.response.Response;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class FilePushResponse extends Response {
+    private static final Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
     @Override
     public String toString() {
         return "FilePushResponse{" +
@@ -12,5 +16,9 @@ public class FilePushResponse extends Response {
                 ", code=" + code +
                 ", message='" + message + '\'' +
                 '}';
+    }
+
+    public String toJson() {
+        return gson.toJson(this);
     }
 }
